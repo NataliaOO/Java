2c9f13dd77b2a5393b14482fdfad199fc7b43f0b
Наталья <44684427+NataliaOO@users.noreply.github.com>
2023-02-24 15:20:06 +0300

    add BD and controller class

---
+++ Chat/src/main/java/main/model/Message.java
@@ -0,0 +1,40 @@
+package main.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private LocalDateTime dateTime;
+    private  String message;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
