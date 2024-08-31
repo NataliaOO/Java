e61f60c4c07a6634030369ed6a0cf3aa1b0f112b
Наталья <44684427+NataliaOO@users.noreply.github.com>
2024-09-01 01:59:17 +0300

    Really Prime Number

--- Java_Bootcamp/Day00/src/ex01/Program.java
+++ Java_Bootcamp/Day00/src/ex01/Program.java
@@ -1,7 +1,19 @@
 package ex01;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if ( n <= 1 ) {
+            System.err.println("Illegal Argument");
+            System.exit(-1);
+        }
+        int i = 2;
+        while (i * i <= n && n % i != 0) {
+            i++;
+        }
+        System.out.println((i * i > n) + " " + --i);
     }
 }
